5a42815c1c7a8a2c0de54bdde654e6b491143c80
lucas-ufrpe <lucas.elias@ufrpe.br>
2024-08-15 18:13:29 +0000

    primeiras modificaçõs

--- src/main/sorter/QuickSorter.java
+++ src/main/sorter/QuickSorter.java
@@ -7,7 +7,21 @@ public class QuickSorter implements Sorter {
     }
 
     public int partition(int[] values, int low, int high) {
-        return 0;
+        int pivo = values[low];
+        int i = high -1;
+        for (int j = high; j < low; j++){
+            if (values[j] <= pivo){
+                i++;
+                int temp = values[i+1];
+                values[i+1] = values[low];
+                values[low] = temp;
+                
+            }
+        }
+        int temp = values[i+1];
+        values[i+1] = values[low];
+        values[low] = temp;
+        return i;
     }
     
 }
