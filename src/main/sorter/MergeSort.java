db1d575fc43eac85be852346def893ca3206b758
lucas-ufrpe <lucas.elias@ufrpe.br>
2024-08-18 18:33:24 +0000

    Entrega do Merge

--- src/main/sorter/MergeSort.java
+++ src/main/sorter/MergeSort.java
@@ -9,6 +9,25 @@ public class MergeSort implements Sorter {
     }
 
     public int[] merge(int[] left, int[] right) {
-        return null;
+        int[]temp = new int [left.length + right.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.length && j < right.length){
+            if (left[i] < right[j]){
+                temp[k++] = left[i++];
+            }
+            else {
+                temp[k++] = right[j++];
+            }
+        }
+        while (i < left.length){
+            temp[k++] = left[i++];
+        }
+        while (j < right.length){
+            temp[k++] = right[j++];
+        }
+        return temp;
     }
 }
